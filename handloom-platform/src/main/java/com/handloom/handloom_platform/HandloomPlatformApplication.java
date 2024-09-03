3b109f3949965e448fd2caf8a02f099f24d51df4
PavanKarthik <karthikpavan370@gmail.com>
2024-09-03 18:42:32 +0530

    Project landing Hello Handloom Displayed

---
+++ handloom-platform/src/main/java/com/handloom/handloom_platform/HandloomPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.handloom.handloom_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandloomPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandloomPlatformApplication.class, args);
+	}
+
+}
---
+++ handloom-platform/src/test/java/com/handloom/handloom_platform/HandloomPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handloom.handloom_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandloomPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
