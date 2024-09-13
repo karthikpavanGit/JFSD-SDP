0131a2d3e86e309ea83ad471c076782b7b4689b8
PavanKarthik <karthikpavan370@gmail.com>
2024-09-13 11:53:25 +0530

    Backend using Spring Initialzer added and base folders Components,pages,redux,services created

---
+++ handloom-backend/src/main/java/com/example/handloom_backend/HandloomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.handloom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandloomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandloomBackendApplication.class, args);
+	}
+
+}
---
+++ handloom-backend/src/test/java/com/example/handloom_backend/HandloomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handloom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandloomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
