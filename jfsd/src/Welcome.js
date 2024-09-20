f6e66e767879ce62fee18ee8ce32552d03db20e4
NavyaSriAlapati <navyaalapatitech@gmail.com>
2024-09-20 18:45:50 +0530

    logo added and ui buttons created for account wishlist and cart

--- jfsd/src/Welcome.js
+++ jfsd/src/Welcome.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './Welcome.css';
+// Import FontAwesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import myImage from './Nakshatra.jpg'; // Import the image
 
 const Welcome = () => {
   return (
     <div className="welcome-container">
       <nav className="navbar">
-        <div className="logo">Handloom Fashion</div>
+        <div>
+          <img src={myImage} alt="Nakshatra Threads" className="account-button" />
+        </div>
+        <div className="logo">Nakshatra Threads</div>
+
         <div className="search-container">
           <input type="text" placeholder="Search..." className="search-bar" />
         </div>
-        <div className="nav-links">
-          <a href="/account" className="nav-link">ACCOUNT</a>
-          <a href="/wishlist" className="nav-link">WISHLIST</a>
-          <a href="/cart" className="nav-link">CART</a>
+
+        {/* Icon Buttons for Account, Wishlist, Cart */}
+        <div className="nav-buttons">
+          <button className="icon-button" onClick={() => window.location.href = '/account'}>
+            <FontAwesomeIcon icon={faUser} /> ACCOUNT
+          </button>
+          <button className="icon-button" onClick={() => window.location.href = '/wishlist'}>
+            <FontAwesomeIcon icon={faHeart} /> WISHLIST
+          </button>
+          <button className="icon-button" onClick={() => window.location.href = '/cart'}>
+            <FontAwesomeIcon icon={faShoppingCart} /> CART
+          </button>
         </div>
       </nav>
     </div>
